4d11c334787f079c9b85d6d18e41b21832276f32
grupoluviah <ads@grupoluviah.com>
2025-11-15 17:41:24 -0300

    script.js

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-  // Scroll suave
   function scrollToContact() {
     document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
   }
-
   function scrollToServices() {
     document.getElementById("services").scrollIntoView({ behavior: "smooth" });
   }
-
 emailjs.init("G5DWAaI7Lndmxn65L");
-
 document.getElementById("contactForm").addEventListener("submit", function (event) {
   event.preventDefault();
-
   const formData = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
     phone: document.getElementById("phone").value,
     message: document.getElementById("message").value,
   }
-
   const serviceID = "service_nrfr48i"
   const templateID = "template_ckm1j2w"
   const submitButton = document.getElementById("submit_button")
   submitButton.textContent = "Enviando..."
   submitButton.disable = true;
-
   emailjs.send(serviceID,templateID, formData)
     .then(() => {
       Toastify({
@@ -35,7 +28,6 @@ document.getElementById("contactForm").addEventListener("submit", function (even
           color: "#f4f4f4"
         }
         }).showToast();
-
         document.getElementById("contactForm").reset();
     })
     .catch((error) => {
@@ -47,13 +39,9 @@ document.getElementById("contactForm").addEventListener("submit", function (even
           color: "#f4f4f4"
         }
         }).showToast();
-
     })
     .finally(() => {
       submitButton.textContent = "Enviar mensagem";
       submitButton.disable = false;
     })
-
-
-
 })
