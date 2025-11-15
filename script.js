336ee0574136d762b0c777c3a63c2c6e9cd15f33
grupoluviah <ads@grupoluviah.com>
2025-11-14 21:46:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToServices() {
+  document.getElementById("services").scrollIntoView({ behavior: "smooth" });
+}
+
